e29f611278049100af71ef05eb4f8445fe012a01
Kevin Dang <kevindouglasdang@gmail.com>
2023-07-24 18:00:15 -0400

    Created a simple notepad prototype

--- server/server.js
+++ server/server.js
@@ -5,16 +5,38 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-let notes = [{name: "Kevin Dang", id: 123}];
+let notes = [];
 
 /**
  * GET Request when the user finds the list of boards
  * from the backend
  */
-app.get('/api', (req, res) => {
+app.get('/api/notes', (req, res) => {
     res.send(notes);
 });
-  
+
+app.get('/api/notes/:id', (req, res) => {
+    const note = notes.find((b) => b.id === req.params.id);
+    if (!note) { // 404 object not found
+        res.status(404).send('The board with the given ID was not found.');
+    }
+    res.send(note);
+});
+
+app.post('/api/notes', (req, res) => {
+    const user = req.body;
+    notes.push(user);
+    res.send(notes);
+});
+
+app.put('/api/notes/:id', (req, res) => {
+    const note = notes.find((b) => b.id === req.params.id);
+    if (!note) { // 404 object not found
+        res.status(404).send('The board with the given ID was not found.');
+    }
+    note.note = req.body.note;
+    res.send(note);
+});
   
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
